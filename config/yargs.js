1d81fdf12de34d8c85215f078f6068ab7d997170
Ricardo Rodriguez <rrodriguez@insideone.com.ar>
2022-04-20 09:54:35 -0300

    Se agregaron las descripciones

--- config/yargs.js
+++ config/yargs.js
@@ -3,14 +3,19 @@ const argv = require("yargs")
 .option("b",{
     alias: 'base',
     type: 'number',
-    demandOption: true
+    demandOption: true,
+    description:"Se establece la base de la tabla de multiplicar"
 }).option("l",{
     alias: "lista",
     type: "boolean",
+    description:"Opcional para listar por consola la tabla creada"
+
 })
 .option("h",{
     alias: "hasta",
     type:"number",
+    description:"Define el numero maximo hasta donde queremos que llegue la tabla"
+
 })
 .check((argv, option)=>{
     if(isNaN(argv.b)){
